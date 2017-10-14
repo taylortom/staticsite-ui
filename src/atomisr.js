a08d56d15cb6e0e390e61fba8fa7f64333ddd1e4
Thomas Taylor <thomas.taylor@kineo.com>
2017-10-14 14:24:30 +0100

    Fix undefined options issue

--- src/atomisr.js
+++ src/atomisr.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 
 module.exports = function Atomisr(filepath, data) {
+  data = data || {};
   var fileContents = fs.readFileSync(path.join(__dirname, 'views', 'atoms', filepath)).toString();
   for(var i = 0, count = Object.keys(data).length; i < count; i++) {
     var key = Object.keys(data)[i];
