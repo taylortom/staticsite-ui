1a04a16096d30f2a3a346520d0be5831fe438c8a
Thomas Taylor <thomas.taylor@kineo.com>
2017-10-14 09:48:41 +0100

    Render files on page render
    And move them under their respective site

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,17 @@
 module.exports = {
   slugify: function(str) {
     return str.replace(/\s|\//g, "-");
+  },
+
+  getIconForFiletype: function(filetype) {
+    switch(filetype) {
+      case 'markdown':
+      case '.md':
+        return 'fa-file-o';
+      case '.html':
+        return 'fa-file-code-o';
+      default:
+        return 'fa-question';
+    }
   }
 }
