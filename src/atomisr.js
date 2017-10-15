9f694114fbe24166a5730b7cf50f6e794a32b91a
Thomas Taylor <thomas.taylor@kineo.com>
2017-10-15 13:29:22 +0100

    Add support for nested attributes in atomisr

--- src/atomisr.js
+++ src/atomisr.js
@@ -4,12 +4,15 @@ var path = require('path');
 module.exports = function Atomisr(filepath, data) {
   data = data || {};
   var fileContents = fs.readFileSync(path.join(__dirname, 'views', 'atoms', filepath)).toString();
-  for(var i = 0, count = Object.keys(data).length; i < count; i++) {
-    var key = Object.keys(data)[i];
-    var value = data[key];
-    fileContents = fileContents.replace(RegExp(`{{${key}}}`, 'g'), value);
-  }
-  // wrap everything in script tags to avoid global scope pollution
+  var templateStrings = fileContents.match(RegExp(`{{.+?}}`, 'g')) || [];
+  templateStrings.forEach(function(templateString) {
+    var keys = templateString.slice(2,-2).split('.');
+    var value = data;
+    // build up the nested value
+    keys.forEach(function(key) { value = value[key]; });
+    fileContents = fileContents.replace(RegExp(`${templateString}`, 'g'), value);
+  });
+  // TODO: automatically wrap everything in script tags to avoid global scope pollution
   // fileContents = fileContents.replace(RegExp('<script type="text\/javascript">([\s\S]*?)<\/script>', 'g'), function() {
   //   console.log('dfsfs', arguments.slice());
   // });
