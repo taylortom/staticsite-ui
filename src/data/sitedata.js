427b03d35f088b807ee94ad308f5510e0a3a0eee
Thomas Taylor <thomas.taylor@kineo.com>
2017-11-09 23:49:24 +0000

    Fix issue with path

--- src/data/sitedata.js
+++ src/data/sitedata.js
@@ -2,5 +2,5 @@ var path = require('path');
 var jsondata = require('./jsondata');
 
 module.exports = function(sitename) {
-  return new jsondata(path.join(__dirname, '..', 'sites', sitename, '_data', 'config.json'));
+  return new jsondata(path.join(__dirname, '..',  '..', 'sites', sitename, '_data', 'config.json'));
 };
