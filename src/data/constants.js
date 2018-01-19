05f3949883bd76af87fdd7d820eb66e0e6cd2964
Thomas Taylor <thomas.taylor@kineo.com>
2018-01-19 19:41:18 +0000

    Amend sidebar to use page titles rather than filenames

--- src/data/constants.js
+++ src/data/constants.js
@@ -3,7 +3,9 @@ var path = require("path");
 module.exports = {
   Root: path.resolve(__dirname, "..", ".."),
   Folders: {
-    Sites: 'sites'
+    Sites: 'sites',
+    Pages: '_pages'
   },
+  DefaultPageFileType: '.md',
   BlankSiteRepo: 'https://github.com/taylortom/staticsite-skele.git'
 };
